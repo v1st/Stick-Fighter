b0ec9a3c40231e8b952228108a49e025befe4e62
v1st <brandon_m918@yahoo.com>
2019-02-20 15:32:17 -0500

    Added player / map collision detection, need to work on slopes

--- client/src/gamefiles/Phaser.js
+++ client/src/gamefiles/Phaser.js
@@ -22,9 +22,12 @@ const config = {
 
 export const game = new Phaser.Game(config);
 
-let platforms;
-let ground;
+let map;
+let tileset;
+let layer;
 let player;
+let gravity = 300;
+let cursors;
 
 function preload() {
   // Preload game assets
@@ -33,9 +36,9 @@ function preload() {
   this.load.tilemapCSV('tileSheet', 'client/src/assets/Tiles/Dino.csv');
   this.load.image('tiles', 'client/src/assets/Tiles/TileMap.png');
   // Load Player sprite
-  this.load.spritesheet('player', 'client/src/assets/Character/png/DinoSpritesBlue.png', {
-    frameWidth: 64,
-    frameHeight: 64
+  this.load.spritesheet('dino', 'client/src/assets/Character/png/DinoSpritesBlue.png', {
+    frameWidth: 24,
+    frameHeight: 24
   });
 }
 
@@ -48,46 +51,77 @@ function create() {
     tileWidth: 32,
     tileHeight: 32
   }
-  const map = this.make.tilemap(config);
-  const tileset = map.addTilesetImage('tiles');
-  const layer = map.createStaticLayer(0, tileset);
-  // map.setCollision();
-  // map.setBaseTileSize(32, 32);
+  // Add tilemap
+  map = this.make.tilemap(config);
+  // Add tile image
+  tileset = map.addTilesetImage('tiles');
+  // Combine tilemap and image
+  layer = map.createStaticLayer(0, tileset,0 ,0);
+  layer.setCollisionBetween(0, 8);
 
   // Create Player model
-  player = this.physics.add.sprite(100, 450, 'player');
+  player = this.physics.add.sprite(100, 450, 'dino');
 
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
+  player.body.setGravityY(gravity)
 
   this.anims.create({
     key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', {
-      start: 0,
-      end: 3
+    frames: this.anims.generateFrameNumbers('dino', {
+      start: 4,
+      end: 9,
     }),
     frameRate: 10,
-    repeat: -1
+    repeat: -1,
   });
 
   this.anims.create({
-    key: 'turn',
-    frames: [{
-      key: 'dude',
-      frame: 4
-    }],
-    frameRate: 20
+    key: 'idle',
+    frames: this.anims.generateFrameNumbers('dino', {
+      start: 0,
+      end: 3
+    }),
+    frameRate: 10
   });
 
   this.anims.create({
     key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', {
-      start: 5,
-      end: 8
+    frames: this.anims.generateFrameNumbers('dino', {
+      start: 4,
+      end: 9
     }),
     frameRate: 10,
     repeat: -1
   });
+
+  // 11  12 kick
+  // 12 - 15 hit
+  // 17 - 22 crouch;
+  // Add collision
+  this.physics.add.collider(player, layer);
+
+  // Add keyboard listener
+  cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update() {}
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+
+    player.anims.play('left', true);
+    player.flipX= true;
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+
+    player.anims.play('right', true);
+    player.flipX= false;
+  } else {
+    player.setVelocityX(0);
+
+    player.anims.play('idle', true);
+  }
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
